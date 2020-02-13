7f7a4aefb7713569d46834da8db9e093789dac6c
santaryw <45605355+santaryw@users.noreply.github.com>
2020-02-13 09:17:54 -0500

    Add files via upload

---
+++ src/com/bill/exampleApi/Example.java
@@ -0,0 +1,16 @@
+package com.bill.exampleApi;
+
+public class Example {
+	public static int sumOfTwo(int a, int b){
+
+		return a+b;
+
+	}
+
+	public static String helloAPI(String name){
+
+		return "Hello "+name+". This is API Sample.";
+
+	}
+}
+ 
